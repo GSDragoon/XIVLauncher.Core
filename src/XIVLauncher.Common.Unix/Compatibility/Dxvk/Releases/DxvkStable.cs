36f7625af1b554fe1a81ee809aa0ed51dc26c243
Greg Seaton <gsdragoon@outlook.com>
2025-09-14 01:37:59 -0400

    Use standard dxvk

--- src/XIVLauncher.Common.Unix/Compatibility/Dxvk/Releases/DxvkStable.cs
+++ src/XIVLauncher.Common.Unix/Compatibility/Dxvk/Releases/DxvkStable.cs
@@ -2,7 +2,7 @@ namespace XIVLauncher.Common.Unix.Compatibility.Dxvk.Releases;
 
 public sealed class DxvkStableRelease : IDxvkRelease
 {
-    public string Name { get; } = "dxvk-gplasync-v2.7-1";
-    public string DownloadUrl { get; } = "https://raw.githubusercontent.com/goatcorp/xlcore-distrib/refs/heads/main/dxvk-gplasync-v2.7-1.tar.gz";
-    public string Checksum { get; } = "1fe59a91f3d3a09a132d1f9c3f7e94d47154e53e58f935daa090aa6dea5c8e6c64800334a6f3014751492a2f32002d8ce63e8086284df0b8d4cbace0353e0f3b";
+    public string Name { get; } = "dxvk-2.7.1";
+    public string DownloadUrl { get; } = "https://github.com/doitsujin/dxvk/releases/download/v2.7.1/dxvk-2.7.1.tar.gz";
+    public string Checksum { get; } = "49f617de31e502fc158df3845845a3c19fa912681dbbf0e2309079184fd9957905350669dcaf1e3ba661f2f1739b5f6b9e1151e73b485021491f194eae9c2d13";
 }
